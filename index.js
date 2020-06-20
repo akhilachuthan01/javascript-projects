6fa2e84be25c9900ca57e1c089764e48cf445ef4
Akhil Achuthan <akhilachuthan01@gmail.com>
2020-06-21 01:32:05 +0530

    Removed files outside Scroll js folder

--- index.js
+++
@@ -1,9 +0,0 @@
-import Scrollbar from 'smooth-scrollbar';
-
-var options = {
-	damping: 0.05,
-	alwaysShowTracks: false,
-};
-
-Scrollbar.init(document.querySelector('#my-scrollbar'), options);
-Scrollbar.init(document.querySelector('#custom'), options);
